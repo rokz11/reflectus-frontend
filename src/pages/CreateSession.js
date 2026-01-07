e9d2f6e63695c1e574f5e345214b1a151394e57e
rokz11 <diplomanter@gmail.com>
2026-01-07 19:07:46 +0100

    Update CreateSession.js

--- src/pages/CreateSession.js
+++ src/pages/CreateSession.js
@@ -67,7 +67,7 @@ export default function CreateSession() {
         preserveAspectRatio="none"
         style={{
           position: "fixed",
-          top: "1px",      // 👈 pod headerjem
+          top: "0.08px",      // 👈 pod headerjem
           left: 0,
           width: "100%",
           height: "180px",   // 👈 pas
@@ -182,6 +182,7 @@ export default function CreateSession() {
     </div>
   );
 }
+
 
 
 
