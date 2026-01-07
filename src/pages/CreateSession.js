c3fe5989b837ee944fdfc7433f220b791fb8166b
rokz11 <diplomanter@gmail.com>
2026-01-07 21:40:06 +0100

    Update CreateSession.js

--- src/pages/CreateSession.js
+++ src/pages/CreateSession.js
@@ -81,7 +81,7 @@ export default function CreateSession() {
         style={{
           width: "100%",
           height: 180,
-          marginBottom: -24,   // ✅ ODSTRANI PRAZEN PROSTOR
+          marginBottom: -20,   // ✅ ODSTRANI PRAZEN PROSTOR
           pointerEvents: "none"
         }}
       >
@@ -181,3 +181,4 @@ export default function CreateSession() {
     </div>
   );
 }
+
